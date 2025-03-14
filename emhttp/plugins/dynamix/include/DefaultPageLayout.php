d30025fda4f253335df6f4cb218b35383c2740b0
Squidly271 <unraidsquid@gmail.com>
2025-03-14 13:07:44 -0400

    Update DefaultPageLayout.php

--- emhttp/plugins/dynamix/include/DefaultPageLayout.php
+++ emhttp/plugins/dynamix/include/DefaultPageLayout.php
@@ -652,7 +652,7 @@ $.ajaxPrefilter(function(s, orig, xhr){
   <div id="header" class="<?=$display['banner']?>">
     <div class="logo">
       <a href="https://unraid.net" target="_blank"><?readfile("$docroot/webGui/images/UN-logotype-gradient.svg")?></a>
-      <unraid-i18n-host><unraid-header-os-version></unraid-header-os-version></unraid-i18n-host>
+      <?=file_get_contents("/etc/unraid-version");?>
     </div>
     <?include "$docroot/plugins/dynamix.my.servers/include/myservers2.php"?>
   </div>
